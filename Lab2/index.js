d0e79b98ff9e044e2d60f11b56b187f82761ad5a
MQ29 <matiax2829@gmail.com>
2024-04-18 13:50:04 +0200

    slider

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,59 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const slides = document.querySelector('.slides');
-    const slideItems = document.querySelectorAll('.slide');
-    const controls = document.querySelectorAll('.control');
+let counter = 1;
 
-    let index = 0;
-    const slideWidth = slideItems[0].clientWidth;
-
-    function goToSlide(x){
-        slides.style.transform = `translateX(-${slideWidth * x}px)`;
-        index = x;
-        updateControls();
-    }
-
-    function updateControls(){
-        controls.forEach((control, x) =>{
-            control.classList.toggle('active', x === index);
-        });
-    }
-
-    controls.forEach((control, x) => {
-        control.addEventListener('click', () => {
-            goToSlide(x);
-        });
-    });
-
-    setInterval(()=>{
-        index = (index + 1) % slideItems.length;
-        goToSlide(index);
-    },3000);
-});
-
-
-// tworzenie img w js-ie
-// const img1 = document.createElement('img')
-// const img2 = new Image()
-// img1.src=""
-
-// odkładanie wykonania kodu w czasie
-// setTimeout(
-//     ()=>{
-//         console.log('Ouc!')
-//         const box = document.querySelector('#slider-inner')
-//         box.style.transform = 'translate(200px,0px)'
-//         setTimeout()
-//     },2_000)
-
-// let positionX = 0
-// const anim = setInterval(
-//     ()=>{
-//         const box = document.querySelector('#slider-inner')
-//         box.style.transform = `translate(${positionX}px,0px)`
-//         positionX++
-// },16)
-
-// przerywanie setInterval
-setTimeout(() => {
-    clearInterval(anim)
-}, 6_000)
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4)
+  {
+    counter = 1;
+  }
+}, 3000);
